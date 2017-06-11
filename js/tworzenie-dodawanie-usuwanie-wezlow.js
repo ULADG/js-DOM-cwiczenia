a2f6d0d196f914443edebdb6cf2e717a3649e3c3
Urszula Dziewit <urszula.dziewit@inwenta.pl>
2017-06-11 10:59:48 +0200

    wezly

---
+++ js/tworzenie-dodawanie-usuwanie-wezlow.js
@@ -0,0 +1,17 @@
+"use strict";
+var istniejacyWezel = document.getElementById('parFirst').children[3];
+console.log(document.getElementById('parFirst').children);
+console.log(istniejacyWezel);
+
+var newElement = document.createElement('p');
+
+var newElementContent = document.createTextNode('To jest nowy paragraf');
+
+newElement.appendChild(newElementContent);
+
+console.log(newElement);
+
+istniejacyWezel.appendChild(newElement);
+console.log(newElement);
+
+istniejacyWezel.removeChild(newElement);
